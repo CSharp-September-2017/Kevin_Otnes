36cc2c5a5a0aeff83782c1d5c558c9389e222fd9
Kevin <kevinot@hotmail.com>
2017-09-23 18:24:01 -0700

    First commit

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            string word = "Hello";
+            bool flag = true;
+            int[] numArray = new int[5]; 
+            List<string> myList =new List<string>();
+            for (int i=0; i < numArray.Length; i++)
+            {
+                    if (i < 4)
+                    {
+                        Console.WriteLine(i);
+                    }
+            }
+            Fizzbuzz(number);
+        }
+
+        public static void Fizzbuzz(int limit)
+        {
+            for (int i = 1; i < limit; i++)
+            {
+                Console.WriteLine(i);
+            }      
+        }
+    }
+}
---
+++ practices/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace practices
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {Dictionary<string,sting> profile = new Dictionary<string, string>();
+        profile.Add("Name","Todd");
+        profile.Add("Location", "Seattle");
+        //loop through dictionay key/value pairs and print the key and fhe value for each (var item in profile)
+        {
+            Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+        }
+        }
+    }
+}
